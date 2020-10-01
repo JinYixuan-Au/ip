f4159dab70edac368b00e610653e90176b6c2f09
JinYixuan-Au <506317808@qq.com>
2020-10-02 02:39:41 +0800

    add exception

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -5,36 +5,62 @@ public class Duke {
     static int thingsCounted = 0;
     static Task[] tasks = new Task[100];
 
-    public static void storeTextAndList() {
+    public static void storeTextAndList() throws DukeException{
         String text;
         Scanner in = new Scanner(System.in);
         text = in.nextLine();
         while (!text.equals("bye")) {
             System.out.println(horizontalLine);
-            if (text.equals("list")) {
-                listTask();
-            } else if (text.contains("done")) {
-                int index = Integer.parseInt(text.substring(5));
-                doneTask(index);
-            } else {
-                if (text.contains("todo")) {
-                    addTodoTask(text);
-                } else if (text.contains("deadline")) {
-                    addDeadlineTask(text);
+            try {
+                if (text.equals("list")) {
+                    listTask();
+                } else if (text.contains("done")) {
+                    int index = Integer.parseInt(text.substring(5));
+                    doneTask(index);
                 } else {
-                    addEventTask(text);
+                    if (text.contains("todo")) {
+                        addTodoTask(text);
+                    } else if (text.contains("deadline")) {
+                        addDeadlineTask(text);
+                    } else if (text.contains("event")) {
+                        addEventTask(text);
+                    } else {
+                        throw new DukeException();
+                    }
                 }
+            }catch (DukeException e) {
+                dealWithException(text);
+            }
                 System.out.println(horizontalLine);
                 text = in.nextLine();
             }
             System.out.println(horizontalLine);
             System.out.println("Bye. Hope to see you again soon");
             System.out.println(horizontalLine);
+    }
+
+    public static void dealWithException(String text){
+        switch (text) {
+            case "todo":
+                System.out.println("The description of a todo cannot be empty.");
+                break;
+            case "deadline":
+                System.out.println("The description of a deadline cannot be empty.");
+                break;
+            case "event":
+                System.out.println("The description of a event cannot be empty.");
+                break;
+            default:
+                System.out.println(("Sorry, I don't know what this means."));
+                break;
         }
     }
 
-    public static void addTodoTask(String text){
+    public static void addTodoTask(String text) throws DukeException{
         String todoDescription;
+        if(text.equals("todo")){
+            throw new DukeException();
+        }
         todoDescription = text.substring(5);
         Task task = new Todo(todoDescription);
         tasks[thingsCounted] = task;
@@ -42,10 +68,13 @@ public class Duke {
         printTask(task);
     }
 
-    public static void addDeadlineTask(String text){
+    public static void addDeadlineTask (String text) throws DukeException{
         String deadlineDescription;
         String deadlineByDate;
         int getIndex;
+        if(text.equals("deadline")){
+            throw new DukeException();
+        }
         getIndex = text.indexOf("/");
         deadlineDescription = text.substring(9, getIndex - 1);
         deadlineByDate = text.substring(getIndex + 4);
@@ -55,10 +84,13 @@ public class Duke {
         printTask(task);
     }
 
-    public static void addEventTask(String text){
+    public static void addEventTask(String text) throws DukeException{
         String eventDescription;
         String eventAtDate;
         int getIndex;
+        if(text.equals("event")){
+            throw new DukeException();
+        }
         getIndex = text.indexOf("/");
         eventDescription = text.substring(6, getIndex - 1);
         eventAtDate = text.substring(getIndex + 4);
@@ -89,7 +121,7 @@ public class Duke {
         System.out.println(horizontalLine);
     }
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws DukeException{
         System.out.println(horizontalLine + "\nHello! I'm Duke\n" + "What can I do for you?\n" + horizontalLine);
         storeTextAndList();
     }
